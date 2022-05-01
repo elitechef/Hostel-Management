4f8be1ee079d8c930e55a05c4d8d34b8d67c2e3a
Shashwat Mishra <70261257+elitechef@users.noreply.github.com>
2022-05-01 14:24:30 +0530

    Add files via upload

---
+++ includes/config.inc.php
@@ -0,0 +1,13 @@
+<?php
+  session_start();
+  $servername = "localhost"; //change this  accordingly
+  $dBUsername = "root";
+  $dBPassword = "";
+  $dBName = "hostel_management_system";
+ // session_start();
+  $conn=mysqli_connect($servername, $dBUsername, $dBPassword, $dBName,"3306");
+
+  if (!$conn) {
+    die("Connection Failed: ".mysqli_connect_error());
+  }
+?>
